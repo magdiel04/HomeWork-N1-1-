dbd0272b50a0400c03a64e1822c3b9b8d6e27a76
Magdiel <magdiel.altynbekov@gmail.com>
2022-11-24 16:44:17 +0600

    Initial Commit

---
+++ HomeWork N1 (1)UITests/HomeWork_N1__1_UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HomeWork_N1__1_UITestsLaunchTests.swift
+//  HomeWork N1 (1)UITests
+//
+//  Created by Magdiel Altynbekov on 24/11/22.
+//
+
+import XCTest
+
+class HomeWork_N1__1_UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

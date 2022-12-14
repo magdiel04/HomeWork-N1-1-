34475ca768cbc580908472d148a1573f3c7488bb
Magdiel <magdiel.altynbekov@gmail.com>
2022-12-14 12:03:29 +0600

    maga

--- HomeWork N1 (1)/ViewController.swift
+++ HomeWork N1 (1)/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var greenView: UIView!
+    
+    @IBOutlet weak var redView: UIView!
+    
+    @IBOutlet weak var orangeView: UIView!
+    
+    @IBOutlet weak var blueView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        greenView.layer.cornerRadius = 40
+        redView.layer.cornerRadius = 40
+        orangeView.layer.cornerRadius = 40
+        blueView.layer.cornerRadius = 40
+        
         // Do any additional setup after loading the view.
     }
 
 
+    
+    
+    
 }
 
